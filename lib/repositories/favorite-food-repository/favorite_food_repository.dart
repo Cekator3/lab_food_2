defd5a9c94b3d77f8bc2d208e7d3285d0f22b4f6
cekator3 <hamsterdreams@inbox.ru>
2024-05-01 11:04:54 +0500

    saving progress

--- lib/repositories/favorite-food-repository/favorite_food_repository.dart
+++ lib/repositories/favorite-food-repository/favorite_food_repository.dart
@@ -1,5 +1,31 @@
-/// A subsystem for interaction with stored data on users favourite foods.
+import 'package:lab_food_2/repositories/favorite-food-repository/DTO/favorite_food_list_item.dart';
+
+/// A subsystem for interaction with stored data on user's favourite foods.
 class FavoriteFoodRepository
 {
+  /// Retrieves a list of user's favourite foods.
+  ///
+  /// Returns empty list if error was encountered.
+  List<FavoriteFoodListItem> getAll()
+  {
+    // ...
+  }
 
+  /// Adds a food to the user's list of favorite foods.
+  void add(int foodId)
+  {
+    // ...
+  }
+
+  /// Removes a food from the user's list of favorite foods.
+  void remove(int foodId)
+  {
+    // ...
+  }
+
+  /// Checks if a food is in the user's list of favorite foods.
+  bool contains(int foodId)
+  {
+    // ...
+  }
 }
--- lib/repositories/food-repository/food_repository.dart
+++ lib/repositories/food-repository/food_repository.dart
@@ -1,5 +1,22 @@
+import 'package:lab_food_2/repositories/food-repository/DTO/food.dart';
+import 'package:lab_food_2/repositories/food-repository/DTO/food_list_item.dart';
+
 /// A subsystem for interaction with stored data on foods.
 class FoodRepository
 {
-    
+  /// Retrieves food's details.
+  ///
+  /// Returns [null] if error was encountered.
+  Food? get(int id)
+  {
+    // ...
+  }
+
+  /// Retrieves a list of foods matching the query.
+  ///
+  /// Returns empty list if error was encountered.
+  List<FoodListItem> find(String query)
+  {
+    // ....
+  }
 }
